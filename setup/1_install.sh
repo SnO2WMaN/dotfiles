7f3bc7889e95405f95bb6b475149c4c36ba8043c
SnO2WMaN <me@sno2wman.net>
2021-07-26 01:45:33 +0900

    install packages

---
+++ setup/1_install.sh
@@ -0,0 +1,20 @@
+yay -S \
+  yay \
+  sway \
+  swaylock-effects \
+  waybar \
+  wofi \
+  slurp grim \
+  pulseaudio \
+  playerctl \
+  brightnessctl \
+  asdf-vm \
+  alacritty \
+  vim \
+  ranger \
+  ghq \
+  fzf \
+  code \
+  firefox-developer-edition \
+  spotify \
+  discord
